72807b590993994c81643ffe012693b53f8a095b
Amanda Magalhaes <mandaamagalhaes@gmail.com>
2023-02-28 16:15:33 -0300

    Para fixar 3

--- fundamentos/secao-03-indrotucao-a-javascript/dia-02-javascript-array-e-loop-for/scripts.js
+++ fundamentos/secao-03-indrotucao-a-javascript/dia-02-javascript-array-e-loop-for/scripts.js
@@ -29,3 +29,13 @@ let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 for(let index = 0; index < groceryList.length; index += 1) {
     console.log(groceryList[index]);
 }
+
+// Para fixar 3:
+
+// Utilize o for/of para imprimir os elementos da lista names com o console.log():
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let name of names){
+    console.log(name);
+}
