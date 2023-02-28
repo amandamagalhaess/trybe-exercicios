2d8241af37bb36d2a20df2b48305e6aa2d75f5f0
Amanda Magalhaes <mandaamagalhaes@gmail.com>
2023-02-28 01:23:54 -0300

    Exercicio 3 - para fixar

--- fundamentos/secao-03-indrotucao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/secao-03-indrotucao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
@@ -19,3 +19,18 @@ let area = base * height;
 
 let perimeter = 5 + 5 + 8 + 8;
   console.log(perimeter);
+
+// Para fixar 3:
+
+let nota = 90;
+
+if(nota >= 80){
+  console.log('Parabéns, você faz parte do grupo de pessoas aprovadas!');
+}
+
+else if(nota >= 60 && nota < 80){
+  console.log('Você está na nossa lista de espera.');
+}
+else{
+  console.log('Infelizmente, você reprovou.');
+}
