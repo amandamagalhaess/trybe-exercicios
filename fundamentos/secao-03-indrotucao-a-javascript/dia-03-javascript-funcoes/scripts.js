1a1de436cced9cc8197e0499c1f26afcb64e2bec
Amanda Magalhaes <mandaamagalhaes@gmail.com>
2023-03-01 20:29:26 -0300

    Para fixar 2

--- fundamentos/secao-03-indrotucao-a-javascript/dia-03-javascript-funcoes/scripts.js
+++ fundamentos/secao-03-indrotucao-a-javascript/dia-03-javascript-funcoes/scripts.js
@@ -22,3 +22,39 @@ console.log(adicionaValor(3000));
 console.log(subtraiValor(1000));
 console.log(multiplicaValor(3/100));
 console.log(divideValor(2));
+
+// Para praticar 2
+
+let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+
+function addClients(name) {
+    if(typeof name === 'string') {
+        clientesTrybeBank.push(name);
+        return 'Cliente adicionado!'
+    }
+    else {
+        return 'Parametro não é do tipo string';
+    }
+}
+
+console.log(addClients('Amanda'));
+console.log(addClients(10));
+console.log(clientesTrybeBank);
+
+function deleteClients(name) {
+    if (typeof name === 'string' && clientesTrybeBank.includes(name)) {
+        clientesTrybeBank.splice(clientesTrybeBank.indexOf(name), 1);
+        return 'Cliente excluído!';
+    }
+    else if (typeof name !== 'string') {
+        return 'Parametro não é do tipo string';
+    }
+    else {
+        return 'Cliente não encontrado';
+    }
+}
+
+console.log(deleteClients('Ada'));
+console.log(deleteClients('Breno'));
+console.log(deleteClients(10));
+console.log(clientesTrybeBank);
