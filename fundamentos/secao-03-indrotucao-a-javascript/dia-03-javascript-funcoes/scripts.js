b9180dadf2fa140dee04d6979b6be25d9f699343
Amanda Magalhaes <mandaamagalhaes@gmail.com>
2023-03-01 15:48:47 -0300

    Para praticar 1

---
+++ fundamentos/secao-03-indrotucao-a-javascript/dia-03-javascript-funcoes/scripts.js
@@ -0,0 +1,24 @@
+// Para praticar 1
+
+let saldo = 5000;
+
+function adicionaValor(valor) {
+    return saldo + valor;
+}
+
+function subtraiValor(valor) {
+    return saldo - valor;
+}
+
+function multiplicaValor(valor) {
+    return saldo * valor;
+}
+
+function divideValor(valor) {
+    return saldo / valor;
+}
+
+console.log(adicionaValor(3000));
+console.log(subtraiValor(1000));
+console.log(multiplicaValor(3/100));
+console.log(divideValor(2));
