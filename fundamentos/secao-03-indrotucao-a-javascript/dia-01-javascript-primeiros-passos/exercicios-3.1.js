07f20e3156262d20c5670777dbbecbe67591cbda
Amanda Magalhaes <mandaamagalhaes@gmail.com>
2023-03-02 18:45:18 -0300

    Segundo exercicio

--- fundamentos/secao-03-indrotucao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-3.1.js
+++ fundamentos/secao-03-indrotucao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-3.1.js
@@ -1,3 +1,5 @@
+// 1
+
 let a = 10;
 let b = 5;
 
@@ -6,3 +8,20 @@ console.log('Subtração = ' + (a - b));
 console.log('Multiplicação = ' + (a * b));
 console.log('Divisão = ' + (a / b));
 console.log('Módulo = ' + (a % b));
+
+//
+
+let num1 = 30;
+let num2 = 56;
+
+// 2
+
+if (num1 > num2) {
+    console.log('Maior número: ' + num1);
+}
+else if (num2 > num1) {
+    console.log('Maior número: ' + num2);
+}
+else {
+    console.log('Os números tem o mesmo valor');
+}
