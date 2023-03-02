61c52c75a954da7951f9624e7d75590476cb525c
Amanda Magalhaes <mandaamagalhaes@gmail.com>
2023-03-02 18:52:10 -0300

    Terceiro exercicio

--- fundamentos/secao-03-indrotucao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-3.1.js
+++ fundamentos/secao-03-indrotucao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-3.1.js
@@ -13,6 +13,7 @@ console.log('Módulo = ' + (a % b));
 
 let num1 = 30;
 let num2 = 56;
+let num3 = 25;
 
 // 2
 
@@ -21,6 +22,21 @@ if (num1 > num2) {
 }
 else if (num2 > num1) {
     console.log('Maior número: ' + num2);
+}
+else {
+    console.log('Os números tem o mesmo valor');
+}
+
+// 3
+
+if (num1 > num2 && num1 > num3) {
+    console.log('Maior número: ' + num1);
+}
+else if (num2 > num1 && num2 > num3) {
+    console.log('Maior número: ' + num2);
+}
+else if (num3 > num1 && num3 > num2) {
+    console.log('Maior número: ' + num3);
 }
 else {
     console.log('Os números tem o mesmo valor');
