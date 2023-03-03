25efa0c4e2523a0d225e5e4daf7ca13c8604a81e
Amanda Magalhaes <mandaamagalhaes@gmail.com>
2023-03-03 02:20:52 -0300

    Decimo primeiro exercicio

--- fundamentos/secao-03-indrotucao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-3.1.js
+++ fundamentos/secao-03-indrotucao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-3.1.js
@@ -172,3 +172,40 @@ if (cost >= 0 && value >= 0) {
 else {
     console.log('Erro: valores não podem ser menores do que 0');
 }
+
+// 11
+
+let grossSalary = 3000;
+let baseSalary;
+let netSalary;
+
+if (grossSalary <= 1556.94) {
+    baseSalary = grossSalary - ((8/100) * grossSalary);
+}
+else if (grossSalary >= 1556.95 && grossSalary <= 2594.92) {
+    baseSalary = grossSalary - ((9/100) * grossSalary);
+}
+else if (grossSalary >= 2594.93 && grossSalary <= 5189.82) {
+    baseSalary = grossSalary - ((11/100) * grossSalary);
+}
+else {
+    baseSalary = grossSalary - 570.88;
+}
+
+if (baseSalary <= 1903.98) {
+    netSalary = baseSalary;
+}
+else if (baseSalary >= 1903.99 && baseSalary <= 2826.65) {
+    netSalary = baseSalary - (((7.5/100) * baseSalary) - 142.80);
+}
+else if (baseSalary >= 2826.66 && baseSalary <= 3751.05) {
+    netSalary = baseSalary - (((15/100) * baseSalary) - 354.80);
+}
+else if (baseSalary >= 3751.06 && baseSalary <= 4664.68) {
+    netSalary = baseSalary - (((22.5/100) * baseSalary) - 636.13);
+}
+else {
+    netSalary = baseSalary - (((27.5/100) * baseSalary) - 869.36);
+}
+
+console.log('Salário líquido: ' + netSalary + ' reais.');
