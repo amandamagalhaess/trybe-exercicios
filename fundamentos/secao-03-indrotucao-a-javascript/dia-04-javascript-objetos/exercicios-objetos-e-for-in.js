159f3ab80b85dc7bb22ba1c726d52395ef923280
Amanda Magalhaes <mandaamagalhaes@gmail.com>
2023-03-08 02:40:33 -0300

    Exercícios - pedido de clientes

--- fundamentos/secao-03-indrotucao-a-javascript/dia-04-javascript-objetos/exercicios-objetos-e-for-in.js
+++ fundamentos/secao-03-indrotucao-a-javascript/dia-04-javascript-objetos/exercicios-objetos-e-for-in.js
@@ -1,8 +1,8 @@
 let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
 
 console.log('Bem vinda, ' + info.personagem);
 
@@ -11,26 +11,50 @@ info['recorrente'] = 'Sim';
 console.log(info);
 
 for (let key in info) {
-    console.log(key);
+  console.log(key);
 }
 
 for (let key in info) {
-    console.log(info[key]);
+  console.log(info[key]);
 }
 
 let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim',
-  };
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
 
 for (let key in info) {
-    if (key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim') {
-        console.log('Ambos recorrentes');
-    }
-    else {
-        console.log(info[key] + ' e ' + info2[key]);
-    }
+  if (key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim') {
+    console.log('Ambos recorrentes');
+  }
+  else {
+    console.log(info[key] + ' e ' + info2[key]);
+  }
 }
 
+// Leitura de objetos
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '"');
+
+leitor.livrosFavoritos.push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+})
+
+console.log('Julia tem ' + leitor.livrosFavoritos.length + ' livros favoritos');
