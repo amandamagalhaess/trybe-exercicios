45ddc29c50c69b38520ae978101a1782b5e14f06
Amanda Magalhaes <mandaamagalhaes@gmail.com>
2023-05-04 13:39:30 -0300

    Exercicios de fixação

---
+++ front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/hello-world-react/src/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class AboutMe extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Amanda Magalhães</h1>
+        <p>Tenho 23 anos e moro em Minas Gerais</p>
+        <h2> Minhas Habilidades</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default AboutMe;
